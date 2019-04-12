8ab877630ffc305518c27a03c1e6a4132bf4d1cb
Nicholas L <33159891+EKKOING@users.noreply.github.com>
2019-04-12 12:03:53 -0400

    unkinow

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ Last updated 25 Feb 2019
 
 // Gets the current date and time
 var d = new Date();
-//var d = new Date(2019,1,4,7,28,1,1);
+//var d = new Date(2019,2,1,8,28,1,1);
 //console.log(" *** d.getDate() " + d.getDate());
 //console.log(" *** d.getMonth() " + d.getMonth());
 //console.log(" *** dayType noClassSats" + i);
@@ -15,7 +15,7 @@ function updateD()
 {
 	"use strict";
 	d = new Date();
-	//d = new Date(2019,1,28,8,27,1,1);
+	//d = new Date(2019,2,1,8,27,1,1);
 }
 
 // Gets the classification of the date, and returns 0 if it is a normal day. Also contains dictionaries for special days.
@@ -327,7 +327,6 @@ function rawToString(raw)
 		else { return toHrs(raw) + " hours " + toMins(raw) + " minutes"; }
 	}
 }
-
 
 setInterval(loadTime, 500);
 setInterval(loadDate, 2000);
