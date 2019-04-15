7b858d50ddf95efebbaaa2fc383c4c999728f2e9
Nicholas L <33159891+EKKOING@users.noreply.github.com>
2019-04-15 14:41:10 -0400

    Add scheduler

--- js/main.js
+++ js/main.js
@@ -11,6 +11,23 @@ var d = new Date();
 //console.log(" *** d.getMonth() " + d.getMonth());
 //console.log(" *** dayType noClassSats" + i);
 
+//Some code off a website to check cookies
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function updateD()
 {
 	"use strict";
