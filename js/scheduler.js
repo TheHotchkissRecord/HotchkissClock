afffb2211b6ff50bf6350c120f2d5a753982909c
Github <nicholas.lorentzen@gmail.com>
2019-04-15 17:11:59 -0400

    separate JS

---
+++ js/scheduler.js
@@ -0,0 +1,33 @@
+
+/*
+Scheduler for Hotchkiss Clock
+Nicholas Lorentzen
+Last updated 20190415
+*/
+var boiyardee = [];
+
+function addCookie(cookname)
+{
+	boiyardee.push(cookname);
+}
+
+function bakeCookies(recipe)
+{
+	addCookie(recipe + "=\"" + document.getElementById(recipe).value + "\"");
+}
+
+function putCookie()
+{
+	var classes = ["m1","m2","m3","m4","m5","m6","m7","m8","t1","t2","t3","t4","t5","t6","t7","t8","w1","w2","w3","w4","y1","y2","y3","y4","y5","y6","y7","y8","f1","f2","f3","f4","f5","f6","f7","f8","s1","s2","s3","s4"];
+
+	classes.forEach(bakeCookies);
+	document.cookie = boiyardee.join("; ") + "path=/index";
+	window.location.replace("http://www.w3schools.com");
+	alert("Cookies Set:" + document.cookie);
+	return true;
+}
+
+function cookiesAlert()
+{
+	window.alert("(STILL IN TESTING) Welcome to the scheduler! In order to use this service you will need cookies enabled. - Nicholas Lorentzen")
+}
