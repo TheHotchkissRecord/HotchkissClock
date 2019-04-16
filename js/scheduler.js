f4abf40063c38c1e9bc1458332095e959f66317f
Github <nicholas.lorentzen@gmail.com>
2019-04-15 22:20:35 -0400

    Add the autofilling capability to the table

--- js/scheduler.js
+++ js/scheduler.js
@@ -37,4 +37,28 @@ function exitScheduler()
 function cookiesAlert()
 {
 	window.alert("(STILL IN TESTING) Welcome to the scheduler! In order to use this service you will need cookies enabled. - Nicholas Lorentzen")
+	var classes = ["m1","m2","m3","m4","m5","m6","m7","m8","t1","t2","t3","t4","t5","t6","t7","t8","w1","w2","w3","w4","y1","y2","y3","y4","y5","y6","y7","y8","f1","f2","f3","f4","f5","f6","f7","f8","s1","s2","s3","s4"];
+	
+	classes.forEach(fillTable);
+}
+
+function fillTable(chef)
+{
+	document.getElementById(chef).value = getCookie(chef);
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return unescape(c.substring(name.length, c.length));
+    }
+  }
+  return "";
 }
